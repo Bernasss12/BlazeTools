7d84b25507fd6c9efd1b6620de1fb9053869f829
Bernasss12 <tudo.pt.minecraft@gmail.com>
2019-05-02 17:33:54 +0100

    Forge Set-Up

---
+++ src/main/java/dev/bernasss12/blazetools/BlazeTools.java
@@ -0,0 +1,31 @@
+package dev.bernasss12.blazetools;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = BlazeTools.MODID, name = BlazeTools.NAME, version = BlazeTools.VERSION)
+public class BlazeTools
+{
+    public static final String MODID = "blazetools";
+    public static final String NAME = "Blaze Tools Mod";
+    public static final String VERSION = "0.0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
